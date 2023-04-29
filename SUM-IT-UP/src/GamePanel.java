ecc2fdd6575139d440269ef77b661add2f00eda8
simonpok <poksimon9@gmail.com>
2023-04-29 09:04:39 +0545

    Game Added

--- SUM-IT-UP/src/GamePanel.java
+++ SUM-IT-UP/src/GamePanel.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class GamePanel {
+import javax.swing.*;
+import java.awt.*;
+
+class GamePanel extends JPanel {
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.setColor(Color.BLACK);
+        g.fillRect(5, 5, 600, 1);
+
+
+
+
+    }
 }
